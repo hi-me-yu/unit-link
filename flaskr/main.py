2b38f6c8b4ec4013eeb6af96532ff26ef1587bc1
yuki <pecotto0731@gmail.com>
2025-02-13 23:40:47 +0900

    2回目main.py

--- flaskr/main.py
+++ flaskr/main.py
@@ -9,7 +9,7 @@ credentials = Credentials.from_service_account_info(service_account_info)
         
 gc = gspread.authorize(credentials)
 
-spreadsheet_id = os.environ["16PQbDNY5ofXSV5uQvgkcjKMZ0TvXdS2K"]
+spreadsheet_id = "16PQbDNY5ofXSV5uQvgkcjKMZ0TvXdS2K"
 
 spreadsheet_url = "https://docs.google.com/spreadsheets/d/1kSXfl_g3WwopEyGIIvswYzNFwotVqh_g1DPNI31gb_w/edit?gid=0#gid=0"
 
